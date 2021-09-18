1c5c6d76cd938b4c762fd6cc11c23aa758fdbeb9
Slothborg <SlothBorg@users.noreply.github.com>
2021-09-17 21:09:59 -0400

    adding tests

--- app/Traits/HasRolesAndPermissions.php
+++ app/Traits/HasRolesAndPermissions.php
@@ -9,29 +9,79 @@ trait HasRolesAndPermissions
 {
     public function permissions()
     {
-        return $this->hasMany(Permission::class);
+        return $this->belongsToMany(Permission::class,'users_permissions');
     }
 
-    /**
-     * @param $permissionName
-     * @return bool
-     */
     public function hasPermission(string $permissionName) : bool
     {
         return (bool) $this->permissions()->where('name', $permissionName)->count();
+//        return in_array($permission, $this->getMergedPermissions());
+    }
+
+    public function getMergedPermissions()
+    {
+        $permissions = array();
+        foreach ($this->getRoles() as $group) {
+            $permissions = array_merge($permissions, $group->permissions()->get()->toArray());
+        }
+        return $permissions;
+    }
+
+    public function attachPermission(Permission $permission) : bool
+    {
+        return $this->permissions()->attach($permission);
+    }
+
+    public function detachPermission(Permission $permission) : bool
+    {
+        return $this->permissions()->detach($permission);
+    }
+
+    public function attachPermissions($permissions) : void
+    {
+        foreach ($permissions as $permission) {
+            $this->attachRole($permission);
+        }
+    }
+
+    public function detachPermissions($permissions) : void
+    {
+        foreach ($permissions as $permission) {
+            $this->detachRole($permission);
+        }
     }
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class,'users_roles');
+        return $this->belongsToMany(Role::class,'role_user');
     }
 
-    /**
-     * @param $roleName
-     * @return bool
-     */
-    public function hasRole(string $roleName)  : bool
+    public function hasRole(string $roleName) : bool
     {
         return (bool) $this->roles()->where('name', $roleName)->count();
+    }
+
+    public function attachRole(Role $role) : bool
+    {
+        return $this->roles()->attach($role);
+    }
+
+    public function detachRole(Role $role) : bool
+    {
+        return $this->roles()->detach($role);
+    }
+
+    public function attachRoles($roles) : void
+    {
+        foreach ($roles as $role) {
+            $this->attachRole($role);
+        }
+    }
+
+    public function detachRoles($roles) : void
+    {
+        foreach ($roles as $role) {
+            $this->detachRole($role);
+        }
     }
 }
--- tests/Unit/PermissionsTest.php
+++ tests/Unit/PermissionsTest.php
@@ -2,10 +2,16 @@
 
 namespace Tests\Unit\Models;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class PermissionsTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -13,7 +19,11 @@ class PermissionsTest extends TestCase
      */
     public function test_users_can_have_roles()
     {
-        $this->assertTrue(false);
+        $user = User::factory()->create();
+        $role = Role::factory()->create();
+        $user->roles()->attach($role->id);
+
+        $this->assertEquals([$role->name], $user->roles()->get()->pluck('name')->toArray());
     }
 
     /**
@@ -23,7 +33,11 @@ class PermissionsTest extends TestCase
      */
     public function test_users_can_have_permissions()
     {
-        $this->assertTrue(false);
+        $user = User::factory()->create();
+        $permission = Permission::factory()->create();
+        $user->permissions()->attach($permission->id);
+
+        $this->assertEquals([$permission->name], $user->permissions()->get()->pluck('name')->toArray());
     }
 
     /**
@@ -33,7 +47,13 @@ class PermissionsTest extends TestCase
      */
     public function test_roles_can_have_permissions()
     {
-        $this->assertTrue(false);
+        $role = Role::factory()->create();
+        $permission1 = Permission::factory()->create();
+        $permission2 = Permission::factory()->create();
+
+        $role->permissions()->attach([$permission1->id, $permission2->id]);
+
+        $this->assertEquals([$permission1->name, $permission2->name], $role->permissions()->get()->pluck('name')->toArray());
     }
 
     /**
@@ -41,11 +61,45 @@ class PermissionsTest extends TestCase
      *
      * @return void
      */
-    public function test_permissions_can_have_roles()
+    public function test_permissions_can_be_attached_to_multiple_roles()
     {
-        $this->assertTrue(false);
+        $permission = Permission::factory()->create();
+        $role1 = Role::factory()->create();
+        $role2 = Role::factory()->create();
+
+        $role1->permissions()->attach($permission->id);
+        $role2->permissions()->attach($permission->id);
+
+        $this->assertEquals([$role1->name, $role2->name], $permission->roles()->get()->pluck('name')->toArray());
     }
 
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_can_check_that_a_user_has_a_given_role()
+    {
+        $user = User::factory()->create();
+        $role = Role::factory()->create();
+        $user->roles()->attach($role->id);
+
+        $this->assertTrue($user->hasRole($role->name));
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_can_check_that_a_user_has_a_given_permission()
+    {
+        $user = User::factory()->create();
+        $permission = Permission::factory()->create();
+        $user->permissions()->attach($permission->id);
+
+        $this->assertTrue($user->hasPermission($permission->name));
+    }
 
     /**
      * A basic test example.
@@ -54,6 +108,16 @@ class PermissionsTest extends TestCase
      */
     public function test_user_can_have_permissions_from_both_roles_and_individual_permissions()
     {
-        $this->assertTrue(false);
+        $user = User::factory()->create();
+        $user->permissions()->attach(Permission::factory()->create()->id);
+        $role = Role::factory()->create();
+        $role->permissions()->attach(Permission::factory()->create()->id);
+        $user->roles()->attach($role->id);
+
+        ddf(
+            $user->allPermissions()->toArray()
+        );
+
+        $this->assertEquals(Permission::all()->pluck('name')->toArray(), $user->permissions()->get()->pluck('name')->toArray());
     }
 }
